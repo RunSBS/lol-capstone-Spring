80af650d65b2e81f0359851ff4be79a524470637
Hyun <dacapo629@naver.com>
2025-10-30 17:28:54 +0900

    필요없는 템플릿파일, 템플릿 컨트롤러제거 /  서머너 컨트롤러의 필요없는 메서드 제거

--- src/main/java/hyun/lol/controller/SummonerController.java
+++ src/main/java/hyun/lol/controller/SummonerController.java
@@ -1,15 +1,10 @@
 package hyun.lol.controller;
 
-import hyun.lol.dto.AccountDto;
-import hyun.lol.dto.LeagueEntryDto;
-import hyun.lol.dto.SummonerDto;
 import hyun.lol.dto.ViewDto;
 import hyun.lol.service.SummonerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,20 +15,5 @@ public class SummonerController {
     @PostMapping("view/{gameName}/{tagLine}")
     public Mono<ViewDto> getViewDto(@PathVariable String gameName, @PathVariable String tagLine) {
         return summonerService.getViewDtoByGameNameAndTagLine(gameName, tagLine);
-    }
-    // 디버깅/로직용 AccountDto 받아 오기
-    @GetMapping("/accountDto/{gameName}/{tagLine}")
-    public Mono<AccountDto> getAccountDtoByGameNameAndTagLine(@PathVariable String gameName, @PathVariable String tagLine) {
-        return summonerService.getAccountDtoByGameNameAndTagLine(gameName, tagLine);
-    }
-    // 디버깅/로직용 SummonerDto 받아 오기
-    @GetMapping("/summonerDto/{puuid}")
-    public Mono<SummonerDto> getSummonerDtoBypuuid(@PathVariable String puuid) {
-        return summonerService.getSummonerDtoByPuuid(puuid);
-    }
-    // 디버깅/로직용 LeagueEntryDto 받아 오기
-    @GetMapping("/leagueEntryDto/{puuid}")
-    public Mono<List<LeagueEntryDto>> getLeagueEntryDtoBypuuid(@PathVariable String puuid) {
-        return summonerService.getLeagueEntryDtoByPuuid(puuid);
     }
 }
