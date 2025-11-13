9b7361fef02882ad617ec29383627f6c22cf9906
Hyun <dacapo629@naver.com>
2025-11-14 00:09:40 +0900

    커뮤니티 TOP 정렬, 내기자B 수정전-투표마감시 글제목 회색, 수정후-투표진행중 흰색으로 반영

--- src/main/java/hyun/db/repo/PostReactionRepository.java
+++ src/main/java/hyun/db/repo/PostReactionRepository.java
@@ -1,7 +1,6 @@
 package hyun.db.repo;
 
 import hyun.db.entity.PostReaction;
-import hyun.db.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
